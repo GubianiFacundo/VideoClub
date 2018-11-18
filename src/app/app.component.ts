3c653dfe0ebd24eabd7ab11bcf00f7a2026b54eb
Facundo <sesshomaru4231@gmail.com>
2018-11-17 23:31:33 -0300

    Lab 4 VideoClub App Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MovieServiceService } from 'src/app/Services/movie-service.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  constructor(private _service:MovieServiceService)
+  {
+    
+  }
+  title = 'Lab 4 - Video Club App';
+}
